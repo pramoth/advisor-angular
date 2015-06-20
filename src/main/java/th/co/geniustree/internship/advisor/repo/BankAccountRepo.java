c8892b6be59fb97d4e4a9be27a6aeb24162e0cfc
metavin007 <meta.vin@hotmail.com>
2015-06-20 12:29:29 +0700

    solve bankaddcount6

--- src/main/java/th/co/geniustree/internship/advisor/repo/BankAccountRepo.java
+++ src/main/java/th/co/geniustree/internship/advisor/repo/BankAccountRepo.java
@@ -7,11 +7,12 @@ package th.co.geniustree.internship.advisor.repo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import th.co.geniustree.internship.advisor.model.BankAccount;
 
 /**
  *
  * @author Jasin007
  */
-public interface BankAccount extends JpaRepository<BankAccount, Integer>{
+public interface BankAccountRepo extends JpaRepository<BankAccount, Integer>{
     
 }
